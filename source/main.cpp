033ba0c79aaaf3d773b5f4f0413fe6e0bb7352e1
nuggets871 <christopherbondier@gmail.com>
2026-03-25 08:12:41 +0100

    tp2 - exo1

--- source/main.cpp
+++ source/main.cpp
@@ -31,28 +31,77 @@ int main()
 {
     // Initialise the micro:bit runtime.
     uBit.init();
-    int count = 0;
+
+
+    //int count = 0;
 
     // Insert your code here!
     //uBit.display.scroll("HELLO WORLD! :)");
+    // uBit.compass.calibrate();
 
-    while(1) {
-        if (uBit.buttonA.isPressed()) {
-            uBit.display.scroll("A");
-        }
-        if (uBit.buttonB.isPressed()) {
-            uBit.display.scroll("B");
-        }
+    // while(1) {
+    //     if (uBit.buttonA.isPressed()) {
+    //         uBit.display.scroll("A");
+    //     }
+    //     if (uBit.buttonB.isPressed()) {
+    //         uBit.display.scroll("B");
+    //     }
 
         //int temp = uBit.thermometer.getTemperature();
         //uBit.display.scroll(temp);
 
-        if (uBit.accelerometer.getX() < -200) {
-            count++;
-        }
-        uBit.display.scroll(count);
+        //if (uBit.accelerometer.getX() < -200) {
+        //    count++;
+        //}
+        //uBit.display.scroll(count);
 
+    //      int heading = uBit.compass.heading();
+
+    //     uBit.display.clear();
+
+    //     if (heading > 315 || heading <= 45) {
+    //         // Nord
+    //         uBit.display.image.setPixelValue(2, 0, 255);
+    //     }
+    //     else if (heading <= 135) {
+    //         // Est
+    //         uBit.display.image.setPixelValue(4, 2, 255);
+    //     }
+    //     else if (heading <= 225) {
+    //         // Sud
+    //         uBit.display.image.setPixelValue(2, 4, 255);
+    //     }
+    //     else {
+    //         // Ouest
+    //         uBit.display.image.setPixelValue(0, 2, 255);
+    //     }
+
+    //     uBit.sleep(200);
+    // }
+
+
+    // TP2 - Exercice 1
+    while (1) {
+        // On set le pin2 vert
+        uBit.io.P2.setDigitalValue(1);
+        uBit.io.P1.setDigitalValue(0);
+        uBit.io.P0.setDigitalValue(0);
+        uBit.sleep(3000);
+
+        // On set le pin1 jaune
+        uBit.io.P2.setDigitalValue(0);
+        uBit.io.P1.setDigitalValue(1);
+        uBit.io.P0.setDigitalValue(0);
+        uBit.sleep(1000);
+
+        // On set le pin0 rouge
+        uBit.io.P2.setDigitalValue(0);
+        uBit.io.P1.setDigitalValue(0);
+        uBit.io.P0.setDigitalValue(1);
+        uBit.sleep(3000);
     }
+
+
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
