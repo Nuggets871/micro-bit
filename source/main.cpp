2b8ab7b3226e6bd6bab7cde381d14b392118f231
nuggets871 <christopherbondier@gmail.com>
2026-04-07 11:55:24 +0200

    code 2

--- source/main.cpp
+++ source/main.cpp
@@ -1,43 +1,30 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2016 British Broadcasting Corporation.
-This software is provided by Lancaster University by arrangement with the BBC.
-
-Permission is hereby granted, free of charge, to any person obtaining a
-copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation
-the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the
-Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
-*/
-
 #include "MicroBit.h"
-#include "neopixel.h"
 
 MicroBit uBit;
 
-int main()
-{
-    // Initialise the micro:bit runtime.
-    uBit.init();
-
-
-
-    // If main exits, there may still be other fibers running or registered event handlers etc.
-    // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
-    // sit in the idle task forever, in a power efficient sleep.
-    release_fiber();
+void onData(MicroBitEvent) {
+    ManagedString s = uBit.radio.datagram.recv();
+    // On ajoute un message de debug pour être sûr
+    uBit.serial.send("PAQUET RECU: ");
+    uBit.serial.send(s + "\r\n");
+    
+    uBit.display.print("!"); // Affiche un point d'exclamation à chaque réception
 }
 
+int main() {
+    uBit.init();
+    uBit.serial.baud(115200);
+    
+    uBit.radio.enable();
+    uBit.radio.setGroup(10); // <--- VERIFIE BIEN QUE C'EST 10 SUR LES DEUX
+    
+    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
+
+    uBit.display.scroll("GW-V2");
+
+    while (1) {
+        // On envoie un petit message au Mac toutes les 2 sec pour prouver que le câble marche
+        uBit.serial.send("En attente de radio...\r\n");
+        uBit.sleep(2000);
+    }
+}
