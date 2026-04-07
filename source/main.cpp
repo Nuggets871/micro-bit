55299ca8933b5f25a3edc4f5220201b8f939e90c
nuggets871 <christopherbondier@gmail.com>
2026-04-07 12:08:08 +0200

    code 2

--- source/main.cpp
+++ source/main.cpp
@@ -2,29 +2,34 @@
 
 MicroBit uBit;
 
-void onData(MicroBitEvent) {
-    ManagedString s = uBit.radio.datagram.recv();
-    // On ajoute un message de debug pour être sûr
-    uBit.serial.send("PAQUET RECU: ");
-    uBit.serial.send(s + "\r\n");
-    
-    uBit.display.print("!"); // Affiche un point d'exclamation à chaque réception
-}
-
 int main() {
     uBit.init();
     uBit.serial.baud(115200);
-    
-    uBit.radio.enable();
-    uBit.radio.setGroup(10); // <--- VERIFIE BIEN QUE C'EST 10 SUR LES DEUX
-    
-    uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
 
-    uBit.display.scroll("GW-V2");
+    // Configuration radio
+    uBit.radio.enable();
+    uBit.radio.setGroup(10);
+    uBit.radio.setFrequencyBand(7);
+    uBit.radio.setTransmitPower(7);
+
+    uBit.display.scroll("GW");
 
     while (1) {
-        // On envoie un petit message au Mac toutes les 2 sec pour prouver que le câble marche
-        uBit.serial.send("En attente de radio...\r\n");
-        uBit.sleep(2000);
+        // Réception radio
+        ManagedString msg = uBit.radio.datagram.recv();
+
+        if (msg.length() > 0) {
+            // Debug USB
+            uBit.serial.send("RECU: ");
+            uBit.serial.send(msg + "\r\n");
+
+            // Feedback visuel
+            uBit.display.print("!");
+        } else {
+            // Debug pour vérifier que ça tourne
+            uBit.serial.send("... attente radio ...\r\n");
+        }
+
+        uBit.sleep(500);
     }
 }
